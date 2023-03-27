606b459fd97fabca676041021e5c1eb9580c8496
Pavel Filippenko <rubikpf2002@mail.ru>
2023-03-27 23:20:30 +0300

    Пофиксил баг со смещением значений

--- lab01/Consistent.cpp
+++ lab01/Consistent.cpp
@@ -12,25 +12,18 @@ double f(double t, double x) {
 double phi(double x) {
     return cos(pi*x);
 }
-
 double psi(double t) {
     return exp(-t);
 }
 
 int main() {
-    
-    // пределы сетки по времени и по пространству
-    double T, X = 0;
-    // количество узлов сетки по времени и по пространству
-    int K, M = 0;
-
     /*
     Задаем T, X, K, M
     */
-    T = 1;
-    X = 1;
-    K = 100;
-    M = 100;
+    double T = 1;
+    double X = 1;
+    int K = 100; // по t
+    int M = 100; // по x
 
     // шаг сетки по времени и по пространству
     double tau = T / K;
@@ -38,10 +31,9 @@ int main() {
 
     double f_arr[K][M];
     double u[K][M];
-    /*Заполнить массив для решения нулями*/
     /*Заполнить массив значений функции в узлах сетки*/
     for (int k = 0; k < K; ++k) {
-        for (int m = 1; m <= M; ++m) {
+        for (int m = 0; m < M; ++m) {
             f_arr[k][m] = f(k*tau, m*h);
         }
     }
@@ -53,24 +45,28 @@ int main() {
     Задаем начальные условия
     */
     for (int k = 0; k < K; ++k) {
-        psi_arr[k] = exp(-k*tau);
+        // psi_arr[k] = exp(-k*tau);
+        psi_arr[k] = psi(k*tau);
     }
+
     for (int m = 0; m < M; ++m) {
         phi_arr[m] = phi(m*h);
     }
 
-
+    // u(0, x) = phi(x)
     for (int i = 0; i < M; ++i) {
         u[0][i] = phi_arr[i];
     }
+    // u(t, 0) = psi(t)
     for (int i = 0; i < K; ++i) {
         u[i][0] = psi_arr[i];
     }
 
     // применение разностной схемы
     for (int k = 0; k < K; ++k) {
-        for (int m = 1; m <= M; ++m) {
-            u[k+1][m] = (f_arr[k][m] + (u[k][m-1] - u[k][m]) / h)*tau + u[k][m];
+        // лол, внимательно смотри, какие точки в расчетной сетке используются
+        for (int m = 1; m < M; ++m) {
+            u[k+1][m] = f_arr[k][m] * tau + (h - tau)/h * u[k][m] + tau/h * u[k][m-1];
         }
     }
 
@@ -78,7 +74,7 @@ int main() {
     FILE* fd = fopen(data_file_name, "a");
     if (fd) {
         for (int k = 0; k < K; ++k) {
-            for (int m = 1; m <= M; ++m) {
+            for (int m = 0; m < M; ++m) {
                 fprintf(fd, "%lf ", u[k][m]);
             }
             fprintf(fd, "\n");
