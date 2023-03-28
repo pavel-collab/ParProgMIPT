490dbfa193c75d36384b7c0b0300ee7a47f11a96
Pavel Filippenko <rubikpf2002@mail.ru>
2023-03-28 15:45:06 +0300

    Вынес часть функционала в отдельные функции

--- lab01/Consistent.cpp
+++ lab01/Consistent.cpp
@@ -3,32 +3,115 @@
 
 #define pi 3.14159265
 
-// Заготовка под функцию
+// Функция, задающая ДУ
 double f(double t, double x) {
     return x + t;
 }
-
-// Заготовка под функции начальных значений
+// Функция, задающая начальное условие на границе t = 0
 double phi(double x) {
     return cos(pi*x);
 }
+// Функция, задающая начальное условие на границе x = 0
 double psi(double t) {
     return exp(-t);
 }
 
-// Функция заполняет значения функции в узлах сетки
-void FillFunctionValues() {
+// Функция возвращает
+// k -- первый индекс двумерного массива (номер строки)
+// m -- второй индекс двумерного массива (номер столбца)
+// M -- количество столбцов в двумерном массиве
+// a[k][m]
+int GetIdx(int k, int m, int M) {
+    return k*M + m;
+}
 
+// Функция заполняет значения функции в узлах сетки
+// f -- указатель на функцию ДУ
+// f_arr -- указатель на массив значений функии в узлах сетки
+// M -- количество точек на пространственном диапазоне
+// K -- количество точек на временном диапазоне
+// tau -- шаг сетки временного диапазона
+// h -- шаг сетки пространственного диапазона
+void FillFunctionValues(double(*f)(double, double), double* f_arr, int M, int K, double tau, double h) {
+for (int k = 0; k < K; ++k) {
+    for (int m = 0; m < M; ++m) {
+            int idx = GetIdx(k, m, M); // индекс одномерного массива
+            f_arr[idx] = f(k*tau, m*h);
+        }
+    }
 }
 
 // Функция заполняет узлы сетки на границе в соответствии с начальными условиями
-void FillInitialValues() {
-
+// f -- указатель на функцию, задающую начальное значение на границе
+// arr -- указатель на массив значений на границе
+// N -- количество точек соответствующего диапазона (временного или пространственного)
+// h -- шаг сетки 
+void FillInitialValues(double(*f)(double), double* arr, int N, double h) {
+    for (int i = 0; i < N; ++i) {
+        arr[i] = f(i*h);
+    }
 }
 
-// Функция возвращает 
-int GetIdx(int k, int m) {
-    return 0;
+// Функция записывает двумерный массив значений в файл
+// data_file_name -- название файла
+// u -- указатель на массив значений размером M*K
+// M -- количество столбцов в двумерном массиве
+// K -- количество строк в двумерном массиве
+void PutData2File(const char* data_file_name, double* u, int M, int K) {
+    FILE* fd = fopen(data_file_name, "a");
+    if (fd) {
+        for (int k = 0; k < K; ++k) {
+            for (int m = 0; m < M; ++m) {
+                int idx = GetIdx(k, m, M);
+                fprintf(fd, "%lf ", u[idx]);
+            }
+            fprintf(fd, "\n");
+        }
+        fclose(fd);
+    }
+}
+
+// Функция производит численно решение задачи с применением схемы: явный левый уголок
+// u -- указатель на массив значений размером M*K
+// f_arr -- указатель на массив значений функии в узлах сетки
+// M -- количество точек на пространственном диапазоне
+// K -- количество точек на временном диапазоне
+// tau -- шаг сетки временного диапазона
+// h -- шаг сетки пространственного диапазона
+void LeftCorner(double* u, double* f_arr, int M, int K, double tau, double h) {
+    for (int k = 0; k < K; ++k) {
+        for (int m = 1; m < M; ++m) {
+            u[GetIdx(k+1, m, M)] = f_arr[GetIdx(k, m, M)] * tau + (h - tau)/h * u[GetIdx(k, m, M)] + tau/h * u[GetIdx(k, m-1, M)];
+        }
+    }
+}
+
+// Функция производит численно решение задачи с применением схемы: явная четырехточечная схема
+void FourPointScheme(double* u, double* f_arr, int M, int K, double tau, double h) {
+    for (int k = 0; k < K; ++k) {
+        for (int m = 1; m < M; ++m) {
+            if (m < M-1)
+                u[GetIdx(k+1, m, M)] = f_arr[GetIdx(k, m, M)] * tau + (tau / (2*h) + tau*tau / (2*h*h)) * u[GetIdx(k, m-1, M)] + (tau*tau / (2*h*h) - tau / (2*h)) * u[GetIdx(k, m+1, M)] + (1 - tau*tau / (h*h)) * u[GetIdx(k, m, M)];
+            else // точки на левой границе считаем схемой левого уголка
+                u[GetIdx(k+1, m, M)] = f_arr[GetIdx(k, m, M)] * tau + (h - tau)/h * u[GetIdx(k, m, M)] + tau/h * u[GetIdx(k, m-1, M)];
+        }
+    }
+}
+
+// Функция производит численно решение задачи с применением схемы: крест
+void CrossScheme(double* u, double* f_arr, int M, int K, double tau, double h) {
+// обсчитываем первый слой схемой уголка
+    for (int m = 1; m < M; ++m) {
+        u[GetIdx(1, m, M)] = f_arr[GetIdx(0, m, M)] * tau + (h - tau)/h * u[GetIdx(0, m, M)] + tau/h * u[GetIdx(0, m-1, M)];
+    }
+    for (int k = 1; k < K; ++k) {
+        for (int m = 1; m < M; ++m) {
+            if (m < M-1) 
+                u[GetIdx(k+1, m, M)] = f_arr[GetIdx(k, m, M)] * 2*tau + u[GetIdx(k-1, m, M)] + (u[GetIdx(k, m-1, M)] - u[GetIdx(k, m+1, M)]) * tau / h;
+            else // точки на левой границе считаем схемой левого уголка
+                u[GetIdx(k+1, m, M)] = f_arr[GetIdx(k, m, M)] * tau + (h - tau)/h * u[GetIdx(k, m, M)] + tau/h * u[GetIdx(k, m-1, M)];
+        }
+    }
 }
 
 int main() {
@@ -52,12 +135,7 @@ int main() {
     double f_arr[K*M];
     double u[K*M];
     /*Заполнить массив значений функции в узлах сетки*/
-    for (int k = 0; k < K; ++k) {
-        for (int m = 0; m < M; ++m) {
-            int idx = k*M + m; // индекс одномерного массива
-            f_arr[idx] = f(k*tau, m*h);
-        }
-    }
+    FillFunctionValues(f, f_arr, M, K, tau, h);
 
     // начальные условия задачи индекс соответствующего одномерного массива по индексам двумерного
     double phi_arr[K];
@@ -65,77 +143,35 @@ int main() {
     /*
     Задаем начальные условия
     */
-    for (int k = 0; k < K; ++k) {
-        // psi_arr[k] = exp(-k*tau);
-        psi_arr[k] = psi(k*tau);
-    }
-
-    for (int m = 0; m < M; ++m) {
-        phi_arr[m] = phi(m*h);
-    }
+    FillInitialValues(psi, psi_arr, K, tau); // значения функции psi(t) в узлах сетки
+    FillInitialValues(phi, phi_arr, M, h); // значения функции phi(t) в узлах сетки
 
     // u(0, x) = phi(x)
     for (int i = 0; i < M; ++i) {
-        u[i] = phi_arr[i];
+        u[GetIdx(0, i, M)] = phi_arr[i];
     }
     // u(t, 0) = psi(t)
     for (int i = 0; i < K; ++i) {
-        u[i*M] = psi_arr[i];
+        u[GetIdx(i, 0, M)] = psi_arr[i];
     }
 
     //---------------------------------------Расчетная схема №1---------------------------------------
     //---------------------------------------Явный левый уголок---------------------------------------
-    // применение разностной схемы
-    for (int k = 0; k < K; ++k) {
-        // лол, внимательно смотри, какие точки в расчетной сетке используются
-        for (int m = 1; m < M; ++m) {
-            u[(k+1)*M + m] = f_arr[k*M + m] * tau + (h - tau)/h * u[k*M + m] + tau/h * u[k*M + m-1];
-        }
-    }
+    // LeftCorner(u, f_arr, M, K, tau, h);
     //------------------------------------------------------------------------------------------------
 
     //---------------------------------------Расчетная схема №2---------------------------------------
     //----------------------------------Явная четырехточечная схема-----------------------------------
-    // for (int k = 0; k < K; ++k) {
-    //     for (int m = 1; m < M; ++m) {
-    //         if (m < M-1)
-    //             u[(k+1)*M + m] = f_arr[k*M + m] * tau + (tau / (2*h) + tau*tau / (2*h*h)) * u[k*M + m-1] + (tau*tau / (2*h*h) - tau / (2*h)) * u[k*M + m+1] + (1 - tau*tau / (h*h)) * u[k*M + m];
-    //         else
-    //             u[(k+1)*M + m] = f_arr[k*M + m] * tau + (h - tau)/h * u[k*M + m] + tau/h * u[k*M + m-1];
-    //     }
-    // }
+    // LeftCorner(u, f_arr, M, K, tau, h);
     //------------------------------------------------------------------------------------------------
 
     //---------------------------------------Расчетная схема №3---------------------------------------
     //---------------------------------------------Крест----------------------------------------------
-    // обсчитываем первый слой схемой уголка
-    //TODO: написать явные выражения для индексов для удобства чтения
-    //! Можно написать функцию для получение индекса соответствующего одномерного массива по индексам двумерного
-    // for (int m = 1; m < M; ++m) {
-    //     u[M + m] = f_arr[m] * tau + (h - tau)/h * u[m] + tau/h * u[m-1];
-    // }
-    // for (int k = 1; k < K; ++k) {
-    //     for (int m = 1; m < M; ++m) {
-    //         if (m < M-1) 
-    //             u[(k+1)*M + m] = f_arr[k*M + m] * 2*tau + u[(k-1)*M + m] + (u[k*M + m-1] - u[k*M + m+1]) * tau / h;
-    //         else
-    //             u[(k+1)*M + m] = f_arr[k*M + m] * tau + (h - tau)/h * u[k*M + m] + tau/h * u[k*M + m-1];
-    //     }
-    // }
+    CrossScheme(u, f_arr, M, K, tau, h);
     //------------------------------------------------------------------------------------------------
 
     const char* data_file_name = "data.txt";
-    FILE* fd = fopen(data_file_name, "a");
-    if (fd) {
-        for (int k = 0; k < K; ++k) {
-            for (int m = 0; m < M; ++m) {
-                int idx = k*M + m;
-                fprintf(fd, "%lf ", u[idx]);
-            }
-            fprintf(fd, "\n");
-        }
-        fclose(fd);
-    }
+    PutData2File(data_file_name, u, M, K);
 
     return 0;
 }
