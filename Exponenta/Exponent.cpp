09139cc6d6c636a132a17c0f10f5f35c05e8a32a
Pavel Filippenko <rubikpf2002@mail.ru>
2023-03-28 17:00:24 +0300

    интегрировал использование длинной арифметики в основную программу. Программа выдает ошибки, нужно внимательно рассмотреть использование длинной арифметики.

--- Exponenta/Exponent.cpp
+++ Exponenta/Exponent.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <gmpxx.h>
+#include <cstring>
 #include <cmath>
 #include "mpi.h"
 
@@ -44,9 +46,9 @@ int main(int argc, char* argv[]) {
 
     int size = 0;
     int rank = 0;
-    int res = 0;
-    double total_res = 0.0;
-    int last_factor = 0;
+    mpf_t total_res;
+    mpf_t last_factor;
+    mpz_t res;
 
     MPI_Status status;
 
@@ -78,26 +80,46 @@ int main(int argc, char* argv[]) {
 
     ItemDestribution(rank, size, N, &rank_start, &rank_end);
 
-    int cur_item = N - rank_start + 1; // начальный элемент ряда
-    int cur_num = N - rank_start + 1;  // текущий множитель в ряде
-    int rank_res = N - rank_start + 1; // частичная сумма каждого поцесса
+    mpz_t cur_item; // начальный элемент ряда
+    mpz_t cur_num; // текущий множитель в ряде
+    mpz_t rank_res; // частичная сумма каждого поцесса
+
+    mpz_init_set_ui(cur_item, N - rank_start + 1);
+    mpz_init_set_ui(cur_num, N - rank_start + 1);
+    mpz_init_set_ui(rank_res, N - rank_start + 1);
 
     for (int i = rank_start; i <= rank_end-1; ++i) {
-        cur_num = cur_num - 1;
-        cur_item = cur_item * cur_num;
-        rank_res += cur_item;
+        mpz_t decriment;
+        mpz_init_set_ui(decriment, 1);
+        mpz_sub(cur_item, cur_item, decriment);
+        mpz_mul(cur_item, cur_item, cur_num);
+        mpz_add(rank_res, rank_res, cur_item);
     }
 
-    int factor = 0;
     if (rank != 0) {
-        MPI_Recv(&factor, 1, MPI_INT, rank-1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        rank_res *= factor;
-        cur_item *= factor;
-        MPI_Send(&rank_res, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        int factor_len = 0;
+        MPI_Recv(&factor_len, 1, MPI_INT, rank-1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        char* str_factor = (char*) calloc(factor_len, sizeof(char));
+        MPI_Recv(str_factor, factor_len, MPI_CHAR, rank-1, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        
+        mpz_t factor;
+        mpz_init_set_str(factor, str_factor, 10);
+        free(str_factor);
+
+        mpz_mul(rank_res, rank_res, factor);
+        mpz_mul(cur_item, cur_item, factor);
+        
+        char* rank_res_str = mpz_get_str(NULL, 10, rank_res);
+        int rank_res_len = strlen(rank_res_str);
+        MPI_Send(&rank_res_len, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&rank_res_str, strlen(rank_res_str), MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 
         if (rank == size-1) {
             // последний процесс посылает нулевому величину N!
-            MPI_Send(&cur_item, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+            char* cur_item_str = mpz_get_str(NULL, 10, cur_item);
+            int cur_item_len = strlen(cur_item_str);
+            MPI_Send(&cur_item_len, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+            MPI_Send(cur_item_str, strlen(cur_item_str), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
         }
     }
 
@@ -106,26 +128,43 @@ int main(int argc, char* argv[]) {
     #endif //DEBUG
 
     if (rank != size-1) {
-        MPI_Send(&cur_item, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
+        char* str_factor_send = mpz_get_str(NULL, 10, cur_item);
+        int factor_send_len = strlen(str_factor_send);
+        MPI_Send(&factor_send_len, 1, MPI_INT, rank+1, 0, MPI_COMM_WORLD);
+        MPI_Send(str_factor_send, strlen(str_factor_send), MPI_CHAR, rank+1, 0, MPI_COMM_WORLD);
     }
 
     if (rank == 0) {
-        int tmp_res;
-        res = rank_res;
+        mpz_t tmp_res;
+        int tmp_res_len;
         for (int proc = 1; proc < size; ++proc) {
-            MPI_Recv(&tmp_res, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            res += tmp_res;
-        }
+            MPI_Recv(&tmp_res_len, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            char* tmp_res_str = (char*) calloc(tmp_res_len, sizeof(char));
+            MPI_Recv(tmp_res_str, tmp_res_len, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            mpz_init_set_str(tmp_res, tmp_res_str, 10);
+            free(tmp_res_str);
 
-        MPI_Recv(&last_factor, 1, MPI_INT, size-1, 1, MPI_COMM_WORLD, &status);
-        total_res = static_cast<double>(res) / static_cast<double>(last_factor);
+            mpz_add(rank_res, rank_res, tmp_res);
+        }
+        char* res_str = mpz_get_str(NULL, 10, rank_res);
+        mpz_init_set_str(res, res_str, 10);
+
+        int last_factor_len;
+        MPI_Recv(&last_factor_len, 1, MPI_INT, size-1, 1, MPI_COMM_WORLD, &status);
+        char* last_factor_str = (char*) calloc(last_factor_len, sizeof(char));
+        MPI_Recv(last_factor_str, last_factor_len, MPI_CHAR, size-1, 1, MPI_COMM_WORLD, &status);
+        mpf_init_set_str(last_factor, last_factor_str, 10);
+        free(last_factor_str);
+
+        mpf_init_set_str(total_res, res_str, 10);
+        mpf_div(total_res, total_res, last_factor);
     }
 
     MPI_Finalize();
 
-    printf("last factor is %d\n", last_factor);
-    printf("result is %d\n", res);
-    printf("result is %f\n", total_res);
-
+    std::cout << "last factor is " << last_factor << std::endl;
+    std::cout << "result is " << res << std::endl;
+    std::cout << "total result is " << total_res << std::endl;
+    
     return 0;
 }
--- Exponenta/tmp1.cpp
+++ Exponenta/tmp1.cpp
@@ -29,27 +29,28 @@ int main(int argc, char* argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if (rank == 0) {
-        mpz_t var;
-        mpz_init_set_ui(var, 124097102);
+        mpz_t var; // заводим целоисленную переменную длинной арифметики
+        mpz_init_set_ui(var, 124097102); // инициализируем эту переменную
+        // mpz_init_set_str(var, "124097102");
         std::cout << var << std::endl;
-        char* str_var = mpz_get_str(NULL, 10, var);
+        char* str_var = mpz_get_str(NULL, 10, var); // конвертируем переменную длинной арифметики в строку
         std::cout << str_var << std::endl;
-        int str_len = strlen(str_var);
-        MPI_Send(&str_len, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-        MPI_Send(str_var, str_len, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+        int str_len = strlen(str_var); // длина строки
+        MPI_Send(&str_len, 1, MPI_INT, 1, 0, MPI_COMM_WORLD); // сначала отсылаем длину строки
+        MPI_Send(str_var, str_len, MPI_CHAR, 1, 0, MPI_COMM_WORLD); // отсылаем строку
         mpz_clear(var);
     }
     else if (rank == 1) {
         int str_len = 0;
-        MPI_Recv(&str_len, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Recv(&str_len, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status); // принимаем длину строки
         std::cout << str_len << std::endl;
-        char* str_var = (char*) calloc(str_len, sizeof(char));
-        MPI_Recv(str_var, str_len, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        char* str_var = (char*) calloc(str_len, sizeof(char)); // выделяем буффер, чтобы принять строку
+        MPI_Recv(str_var, str_len, MPI_CHAR, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);  //прнимаем строку
         std::cout << str_var << std::endl;
         
-        mpz_t var;
-        mpz_init(var);
-        mpz_set_str(var, str_var, 10);
+        mpz_t var; // заводим новую переменную длинной арифметики
+        mpz_init(var); // инициализируем ее
+        mpz_set_str(var, str_var, 10); // присваем ей значение через строку
         std::cout << var << std::endl;
         mpz_t addition;
         mpz_init_set_ui(addition, 20);
