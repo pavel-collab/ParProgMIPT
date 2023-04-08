be12100ee54b45b2fa389a309f678883fdc9191b
Pavel Filippenko <rubikpf2002@mail.ru>
2023-04-08 11:33:39 +0300

    Compleat basik work with files

--- WorkWithFiles/main.cpp
+++ WorkWithFiles/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char* argv[]) {
 
     int size = 0;
     int rank = 0;
+    int offset = 0;
 
     MPI_Status status;
 
@@ -22,9 +23,9 @@ int main(int argc, char* argv[]) {
     MPI_File data_file;
     MPI_File_open(MPI_COMM_WORLD, "data.txt", MPI_MODE_CREATE | MPI_MODE_WRONLY | MPI_MODE_APPEND, MPI_INFO_NULL, &data_file);
 
-    const char* my_rank = std::to_string(rank).c_str();
+    const char* my_rank = (std::to_string(rank) + " ").c_str();
     // установка смещения
-    MPI_File_set_view(data_file, rank*strlen(my_rank)*sizeof(char), MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
+    MPI_File_set_view(data_file, (size-rank-1)*strlen(my_rank)*sizeof(char), MPI_CHAR, MPI_CHAR, "native", MPI_INFO_NULL);
     MPI_File_write(data_file, my_rank, strlen(my_rank), MPI_CHAR, MPI_STATUS_IGNORE);
     
     MPI_File_close(&data_file);
