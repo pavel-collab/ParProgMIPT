0a31ac4b8869a53fc6759da60dfb7c8868f3a408
Pavel Filippenko <rubikpf2002@mail.ru>
2023-04-22 12:17:09 +0300

    New stack implrmrntaion

--- Integration/Stack.hpp
+++ Integration/Stack.hpp
@@ -10,34 +10,27 @@ public:
     Stack() = default;
     // Инициализация с первым "элементом" стека
     Stack(double A, double B, double fa, double fb) {
-        this->m_A[idx] = A;
-        this->m_B[idx] = B;
-        this->m_fa[idx] = fa;
-        this->m_fb[idx] = fb;
-        this->idx++;
+        this->Push(A, B, fa, fb);
     }
     ~Stack() = default;
 
     void Push(double A, double B, double fa, double fb) {
-        this->m_A.emplace_back(A);
-        this->m_B.emplace_back(B);
-        this->m_fa.emplace_back(fa);
-        this->m_fb.emplace_back(fb);
+        std::unordered_map<std::string, double> node;
+        node.insert({"A", A});
+        node.insert({"B", B});
+        node.insert({"fa", fa});
+        node.insert({"fb", fb});
+        this->m_stack.emplace_back(node);
         this->idx++;
     }
 
-    // void Top()
+    std::unordered_map<std::string, double> Top() {
+        return this->m_stack[this->idx-1];
+    }
 
-    void Pop(double* buf) {
+    void Pop() {
         this->idx--;
-        buf[0] = this->m_A[idx];
-        buf[1] = this->m_B[idx];
-        buf[2] = this->m_fa[idx];
-        buf[3] = this->m_fb[idx];
-        this->m_A.pop_back();
-        this->m_B.pop_back();
-        this->m_fa.pop_back();
-        this->m_fb.pop_back();
+        this->m_stack.pop_back();
     }
 
     unsigned int Size() {
@@ -51,19 +44,15 @@ public:
     }
 
 private:
-    std::vector<double> m_A;
-    std::vector<double> m_B;
-    std::vector<double> m_fa;
-    std::vector<double> m_fb;
-
+    std::vector<std::unordered_map<std::string, double>> m_stack;
     unsigned int idx = 0;
 
     void PrintElement(unsigned int i) {
         printf("Stack [%u]\n", i);
-        printf("\tA[%u]  = %lf\n", i, this->m_A[i]);
-        printf("\tB[%u]  = %lf\n", i, this->m_B[i]);
-        printf("\tfa[%u] = %lf\n", i, this->m_fa[i]);
-        printf("\tfb[%u] = %lf\n", i, this->m_fb[i]);
+        std::unordered_map<std::string, double> node = this->Top();
+        for (auto item : node) {
+            std::cout << item.first << "[" << i << "] = " << item.second << std::endl;
+        }
         printf("\n");
     }
 };
